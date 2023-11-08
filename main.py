68a9b8450b24909b7f539eb5941f144baab24af2
Mario Vergara <mvergaral@pelambres.cl>
2023-11-08 01:14:53 -0300

    generated a src and some functions

--- main.py
+++ main.py
@@ -4,22 +4,15 @@ import datetime
 from openai import OpenAI
 from dotenv import load_dotenv
 
-def save_info(dict_info, now):
 
-    date_path = now.strftime('%Y-%m')
-    time_filename = now.strftime('%Y-%m-%dT%H:%M:%S') + '.json'
+from src.data import JSONDataManager
+from src.path import PATH_DATA
 
-    folder_path = os.path.join('data', date_path)
-    os.makedirs(folder_path, exist_ok=True)
+db = JSONDataManager(base_path=PATH_DATA)
 
-    file_path = os.path.join(folder_path, time_filename)
-
-    with open(file_path, 'w') as json_file:
-        json.dump(dict_info, json_file, indent=4)
 
 if __name__ == '__main__':
     now =  datetime.datetime.utcnow()
-
     load_dotenv()
     api_key = os.environ.get("OPENAI_API_KEY")
 
@@ -46,4 +39,4 @@ if __name__ == '__main__':
 
     info["content"] = completion.choices[0].message.content
 
-    save_info(dict_info=info, now=now)
+    db.save_info(dict_info=info, now=now)
