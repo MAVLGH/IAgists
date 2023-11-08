cdf414d3a852560112e0ae98f2957e45690f919e
Mario Vergara <mvergaral@pelambres.cl>
2023-11-08 00:35:02 -0300

    updated dependencies

--- Makefile
+++ Makefile
@@ -1,3 +1,4 @@
 install-local:
+	python -m pip install --upgrade pip
 	pip install -r requirements.txt
 	pip freeze > req_freeze.txt
--- main.py
+++ main.py
@@ -1,8 +1,19 @@
+import os
 from openai import OpenAI
+from dotenv import load_dotenv
 
-client = OpenAI()
-# defaults to getting the key using os.environ.get("OPENAI_API_KEY")
-# if you saved the key under a different environment variable name, you can do something like:
-# client = OpenAI(
-#   api_key=os.environ.get("CUSTOM_ENV_NAME"),
-# )
+load_dotenv()
+
+client = OpenAI(
+   api_key=os.environ.get("OPENAI_API_KEY"),
+ )
+
+completion = client.chat.completions.create(
+  model="gpt-3.5-turbo",
+  messages=[
+    {"role": "system", "content": "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair."},
+    {"role": "user", "content": "Compose a poem that explains the concept of recursion in programming."}
+  ]
+)
+
+print(completion.choices[0].message)
